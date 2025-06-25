22db9e8ab8592383104d5a5b94e5a1c684dfad11
Gowtham Reddy <gowthamreddy3010@gmail.com>
2025-06-25 13:40:18 +0530

    This is Schema

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,12 +1,18 @@
 import {Todo} from "./todoController.js";
 //create a Todo
-const createTodo =async ()=>{
+const createTodo =async (req,res)=>{
+    try{
+       
+    
     const {title,description}=req.body;
     const newTodo = await Todo.create({
         title,
         description
     })
     resizeBy.status(201).json(newTodo);
+}catch(error){
+    res.status(404).json({message:error.message})
+}
 }
 
 
