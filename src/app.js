ce3d3b1eaee544ec7e4228d7710045f895d0ac9c
Gowtham Reddy <gowthamreddy3010@gmail.com>
2025-06-25 13:56:40 +0530

    This the schema

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
-
-
 const app = express();
+
+import todoRoutes from "./routes/todoRoutes.js"
 app.use(express.json());
+app.use('/api/v1/todos',todoRoutes);
+
 
 
 
---
+++ src/routes/todoRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import {createTodo} from "../controllers/todoController.js";
+const router = express.Router();
+//routes
+router.post('/',createTodo);
+export default router;
